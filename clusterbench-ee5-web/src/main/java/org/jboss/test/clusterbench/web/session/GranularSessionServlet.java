c777ac59501e9349ab6db693c06e45216ab3a399
Radoslav Husar <radosoft@gmail.com>
2011-12-06 17:55:41 +0100

    backported servlets to EE5 version of the app, updated web xml to EE5

---
+++ clusterbench-ee5-web/src/main/java/org/jboss/test/clusterbench/web/session/GranularSessionServlet.java
@@ -0,0 +1,47 @@
+package org.jboss.test.clusterbench.web.session;
+
+import org.jboss.test.clusterbench.common.SerialBean;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class GranularSessionServlet extends HttpServlet {
+
+    private static final Logger log = Logger.getLogger(GranularSessionServlet.class.getName());
+    public static final String KEY_SERIAL = GranularSessionServlet.class.getName() + "Serial";
+    public static final String KEY_CARGO = GranularSessionServlet.class.getName() + "Cargo";
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession(true);
+
+        if (session.isNew()) {
+            log.log(Level.INFO, "New session created: {0}", session.getId());
+            // Reuse serial bean logic to generate the data.
+            SerialBean tempBean = new SerialBean();
+            session.setAttribute(KEY_SERIAL, tempBean.getSerial());
+            session.setAttribute(KEY_CARGO, tempBean.getCargo());
+        }
+
+        Integer serial = (Integer) session.getAttribute(KEY_SERIAL);
+        // Do nothing with cargo?
+        byte[] cargo = (byte[]) session.getAttribute(KEY_CARGO);
+
+        resp.setContentType("text/plain");
+
+        // Now store it in an attribute
+        session.setAttribute(KEY_SERIAL, serial + 1);
+
+        resp.getWriter().print(serial);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Servlet using HTTP Session attributes to store serial and cargo separately.";
+    }
+}
