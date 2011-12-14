86ab377de44fa8fe6eaeae62970dfbaf31ab9721
Michal Babacek <michal.babacek@gmail.com>
2011-12-14 14:15:23 +0100

    Moved CPU load logic from clusterbench-ee6-web to clusterbench-common

---
+++ clusterbench-common/src/main/java/org/jboss/test/clusterbench/common/load/AverageSystemLoad.java
@@ -0,0 +1,78 @@
+package org.jboss.test.clusterbench.common.load;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * 
+ * @author Michal Babacek <mbabacek@redhat.com> 
+ *
+ * @see org.jboss.test.clusterbench.web.load.AverageSystemLoadServlet
+ */
+public class AverageSystemLoad {
+   private static final Logger log = Logger.getLogger(AverageSystemLoad.class.getName());
+
+   private List<LoadRunner> loadRunners = new ArrayList<LoadRunner>();
+   private long operationStarted = 0L;
+   
+   public String spawnLoadThreads(int numberOfThreads, int milliseconds) {
+      operationStarted = System.currentTimeMillis();
+      for(int i = 0; i < numberOfThreads; i++) {
+         loadRunners.add(new LoadRunner(milliseconds));
+      }
+      //Wait till we are done
+      while(stillActiveRunners() != 0) {
+         try {
+            Thread.sleep(50);
+         } catch (InterruptedException e) {
+            log.log(Level.SEVERE, "Whoops, our monitoring thread has been interrupted.");
+         }
+      }
+     return "DONE, I was stressing CPU with "+numberOfThreads+" evil threads for "+(System.currentTimeMillis()-operationStarted)+" milliseconds (including warm-up).";
+   }
+   
+   private int stillActiveRunners() {
+      int stillActiveRunners = 0;
+      for(LoadRunner loadRunner : loadRunners) {
+         if(!loadRunner.isComplete()) {
+            stillActiveRunners++;
+         }
+      }
+      return stillActiveRunners;
+   }
+   
+   private class LoadRunner implements Runnable {
+
+      private int milliseconds;
+      private boolean isComplete = false;
+      private Thread runner;
+      
+      public LoadRunner(int milliseconds) {
+         this.milliseconds = milliseconds;
+         runner = new Thread(this);
+         runner.start();
+      }
+
+      @Override
+      public void run() {
+         // we go with nanos
+         long howLong = milliseconds * 1000000L;
+         long startTime = System.nanoTime();
+         while ((System.nanoTime() - startTime) < howLong) {
+           //Le wild empty loop :-P
+         }
+         setComplete(true);
+      }
+
+      public synchronized void setComplete(boolean isComplete) {
+         this.isComplete = isComplete;
+      }
+
+      public synchronized boolean isComplete() {
+         return isComplete;
+      }
+     
+   }
+}
--- clusterbench-ee6-web/src/main/java/org/jboss/test/clusterbench/web/load/AverageSystemLoadServlet.java
+++ clusterbench-ee6-web/src/main/java/org/jboss/test/clusterbench/web/load/AverageSystemLoadServlet.java
@@ -1,9 +1,6 @@
 package org.jboss.test.clusterbench.web.load;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.servlet.ServletException;
@@ -12,10 +9,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.jboss.test.clusterbench.common.load.AverageSystemLoad;
+
 /**
  * AverageSystemLoadServlet
  * 
- * @author Michal Babacek <mbabacek@redhat.com> 
+ * @author Michal Babacek 
  *
  * This simple servlet is used for stressing the server's CPU.
  * 
@@ -25,94 +24,28 @@ import javax.servlet.http.HttpServletResponse;
  * By doing so, there will be 4 threads with evil active-loops created.
  * These threads will be running for 20000 milliseconds.
  * 
- * There are two responses you can get:
+ * After the aforementioned time, you shall receive a response saying something like:
  * 
- * 1) If there are no active load runners at the moment:
- * LoadRunner #0 has been created.
- * LoadRunner #1 has been created.
- * LoadRunner #2 has been created.
- * LoadRunner #3 has been created.
+ * DONE, I was stressing CPU with 4 evil threads for 20000 milliseconds (including warm-up).
  * 
- * 2) If there are active runners (CPU is being stressed at the moment):
- * INFO:There are still 4 active runners that have been running for 700 milliseconds.
- * 
+ * NOTE: Do not forget to set some reasonable time-out on your client...
  * That's it. No more functionality.
  */
 @WebServlet(name = "AverageSystemLoadServlet", urlPatterns = { "/averagesystemload" })
 public class AverageSystemLoadServlet extends HttpServlet {
    private static final Logger log = Logger.getLogger(AverageSystemLoadServlet.class.getName());
-   private List<LoadRunner> loadRunners = new ArrayList<LoadRunner>();
-   private long operationStarted = 0L;
+   private final AverageSystemLoad averageSystemLoad = new AverageSystemLoad();
    
    @Override
    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
       int milliseconds = Integer.parseInt(request.getParameter("milliseconds"));
       int numberOfThreads = Integer.parseInt(request.getParameter("threads")); 
       response.setContentType("text/plain");
-      response.getWriter().print(spawnLoadThreads(numberOfThreads, milliseconds));
+      response.getWriter().print(averageSystemLoad.spawnLoadThreads(numberOfThreads, milliseconds));
    }
 
    @Override
    public String getServletInfo() {
       return "By invoking AverageSystemLoadServlet, you stress CPU.";
-   }
-
-   public String spawnLoadThreads(int numberOfThreads, int milliseconds) {
-      operationStarted = System.currentTimeMillis();
-      for(int i = 0; i < numberOfThreads; i++) {
-         loadRunners.add(new LoadRunner(milliseconds));
-      }
-      //Wait till we are done
-      while(stillActiveRunners() != 0) {
-         try {
-            Thread.sleep(50);
-         } catch (InterruptedException e) {
-            log.log(Level.SEVERE, "Whoops, our monitoring thread has been interrupted.");
-         }
-      }
-     return "DONE, I was stressing CPU with "+numberOfThreads+" evil threads for "+(System.currentTimeMillis()-operationStarted)+" milliseconds (including warm-up).";
-   }
-   
-   private int stillActiveRunners() {
-      int stillActiveRunners = 0;
-      for(LoadRunner loadRunner : loadRunners) {
-         if(!loadRunner.isComplete()) {
-            stillActiveRunners++;
-         }
-      }
-      return stillActiveRunners;
-   }
-   
-   private class LoadRunner implements Runnable {
-
-      private int milliseconds;
-      private boolean isComplete = false;
-      private Thread runner;
-      
-      public LoadRunner(int milliseconds) {
-         this.milliseconds = milliseconds;
-         runner = new Thread(this);
-         runner.start();
-      }
-
-      @Override
-      public void run() {
-         // we go with nanos
-         long howLong = milliseconds * 1000000L;
-         long startTime = System.nanoTime();
-         while ((System.nanoTime() - startTime) < howLong) {
-           //Le wild empty loop :-P
-         }
-         setComplete(true);
-      }
-
-      public synchronized void setComplete(boolean isComplete) {
-         this.isComplete = isComplete;
-      }
-
-      public synchronized boolean isComplete() {
-         return isComplete;
-      }
-     
    }
 }
