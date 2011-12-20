e37f78e70f9a3ed59226fb28696d9ada71ea93e3
Radoslav Husar <radosoft@gmail.com>
2011-12-20 12:52:18 +0100

    added back @Clustered to @Singleton beans

--- clusterbench-ee6-ejb/src/main/java/org/jboss/test/clusterbench/ejb/singleton/LocalSingletonSB.java
+++ clusterbench-ee6-ejb/src/main/java/org/jboss/test/clusterbench/ejb/singleton/LocalSingletonSB.java
@@ -3,8 +3,8 @@ package org.jboss.test.clusterbench.ejb.singleton;
 import javax.ejb.Singleton;
 import javax.ejb.LocalBean;
 import javax.ejb.Remove;
+import org.jboss.ejb3.annotation.Clustered;
 import org.jboss.test.clusterbench.common.SerialBean;
-//import org.jboss.ejb3.annotation.Clustered; -- dropped in AS 7.1 release.
 
 /**
  * Scope interface javax.enterprise.context.SessionScoped is not allowed on singleton enterprise beans.
@@ -12,7 +12,7 @@ import org.jboss.test.clusterbench.common.SerialBean;
  */
 @Singleton
 @LocalBean
-//@Clustered -- dropped in AS 7.1 release.
+@Clustered
 public class LocalSingletonSB {
 
     private SerialBean bean;
