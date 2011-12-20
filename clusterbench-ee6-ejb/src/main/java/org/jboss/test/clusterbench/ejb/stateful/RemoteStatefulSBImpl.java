820d93092f17f07801a5b9a81fab9778b1faf18c
Radoslav Husar <radosoft@gmail.com>
2011-12-20 14:58:36 +0100

    AS7-3018 -- implements Serializable

--- clusterbench-ee6-ejb/src/main/java/org/jboss/test/clusterbench/ejb/stateful/RemoteStatefulSBImpl.java
+++ clusterbench-ee6-ejb/src/main/java/org/jboss/test/clusterbench/ejb/stateful/RemoteStatefulSBImpl.java
@@ -1,13 +1,20 @@
 package org.jboss.test.clusterbench.ejb.stateful;
 
+import java.io.Serializable;
 import javax.ejb.Remove;
 import javax.ejb.Stateful;
-import org.jboss.test.clusterbench.common.SerialBean;
 import org.jboss.ejb3.annotation.Clustered;
+import org.jboss.test.clusterbench.common.SerialBean;
 
+/**
+ * Serializable? https://issues.jboss.org/browse/AS7-3018
+ * 
+ * @author Radoslav Husar
+ * @version Dec 2011
+ */
 @Stateful
 @Clustered
-public class RemoteStatefulSBImpl implements RemoteStatefulSB {
+public class RemoteStatefulSBImpl implements RemoteStatefulSB, Serializable {
 
     private SerialBean bean;
 
