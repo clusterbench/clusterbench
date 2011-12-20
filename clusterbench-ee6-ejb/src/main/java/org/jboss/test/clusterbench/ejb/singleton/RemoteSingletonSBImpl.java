7d82b4330a034be2e3dee5c4e72e3ce6d4a2deb5
Radoslav Husar <radosoft@gmail.com>
2011-12-20 12:52:18 +0100

    added back @Clustered to @Singleton beans

--- clusterbench-ee6-ejb/src/main/java/org/jboss/test/clusterbench/ejb/singleton/RemoteSingletonSBImpl.java
+++ clusterbench-ee6-ejb/src/main/java/org/jboss/test/clusterbench/ejb/singleton/RemoteSingletonSBImpl.java
@@ -3,12 +3,12 @@ package org.jboss.test.clusterbench.ejb.singleton;
 import javax.ejb.Singleton;
 import javax.ejb.LocalBean;
 import javax.ejb.Remove;
+import org.jboss.ejb3.annotation.Clustered;
 import org.jboss.test.clusterbench.common.SerialBean;
-//import org.jboss.ejb3.annotation.Clustered; -- dropped in AS 7.1 release.
 
 @Singleton
 @LocalBean
-//@Clustered -- dropped in AS 7.1 release.
+@Clustered
 public class RemoteSingletonSBImpl implements RemoteSingletonSB {
 
     private SerialBean bean;
