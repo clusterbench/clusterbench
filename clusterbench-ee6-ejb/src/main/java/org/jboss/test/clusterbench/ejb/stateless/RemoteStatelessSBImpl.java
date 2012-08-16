f611b97cad21ca0d874b148ce45eb50546f443ce
Radoslav Husar <radosoft@gmail.com>
2012-08-16 12:49:18 +0200

    fix RemoteStatelessSBImpl interface

--- clusterbench-ee6-ejb/src/main/java/org/jboss/test/clusterbench/ejb/stateless/RemoteStatelessSBImpl.java
+++ clusterbench-ee6-ejb/src/main/java/org/jboss/test/clusterbench/ejb/stateless/RemoteStatelessSBImpl.java
@@ -2,10 +2,9 @@ package org.jboss.test.clusterbench.ejb.stateless;
 
 import javax.ejb.Stateless;
 import org.jboss.ejb3.annotation.Clustered;
-import org.jboss.test.clusterbench.common.ejb.CommonStatelessSB;
 import org.jboss.test.clusterbench.common.ejb.CommonStatelessSBImpl;
 
 @Stateless
 @Clustered
-public class RemoteStatelessSBImpl extends CommonStatelessSBImpl implements CommonStatelessSB {
+public class RemoteStatelessSBImpl extends CommonStatelessSBImpl implements RemoteStatelessSB {
 }
