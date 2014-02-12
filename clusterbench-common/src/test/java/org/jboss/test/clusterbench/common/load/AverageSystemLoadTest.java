50497f2419229b95d0bd2b324651fe063931a202
Radoslav Husar <radosoft@gmail.com>
2014-02-12 19:58:34 +0100

    minor formatting change

--- clusterbench-common/src/test/java/org/jboss/test/clusterbench/common/load/AverageSystemLoadTest.java
+++ clusterbench-common/src/test/java/org/jboss/test/clusterbench/common/load/AverageSystemLoadTest.java
@@ -21,23 +21,21 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 /**
- * 
- * @author Michal Babacek 
- *
+ * @author Michal Babacek
  */
 public class AverageSystemLoadTest {
 
-   @Test
-   public void spawnLoadThreadsTest() {
-      AverageSystemLoad averageSystemLoad = new AverageSystemLoad();
-      int duration = 2000;
-      int threads = 2;
-      long started = System.currentTimeMillis();
-      String result = averageSystemLoad.spawnLoadThreads(threads, duration);
-      long ended = System.currentTimeMillis();
-      String infoMessgae = "DONE, I was stressing CPU with " + threads + " evil threads for ";
-      assertTrue("This message [" + infoMessgae + "] has to be a substring of [" + result + "].", result.contains(infoMessgae));
-      long took = ended - started;
-      assertTrue("CPU stressing took less time than it should have. Was:" + took + ", expected at least:" + duration, took >= duration);
-   }
+    @Test
+    public void spawnLoadThreadsTest() {
+        AverageSystemLoad averageSystemLoad = new AverageSystemLoad();
+        int duration = 2000;
+        int threads = 2;
+        long started = System.currentTimeMillis();
+        String result = averageSystemLoad.spawnLoadThreads(threads, duration);
+        long ended = System.currentTimeMillis();
+        String infoMessage = "DONE, I was stressing CPU with " + threads + " evil threads for ";
+        assertTrue("This message [" + infoMessage + "] has to be a substring of [" + result + "].", result.contains(infoMessage));
+        long took = ended - started;
+        assertTrue("CPU stressing took less time than it should have. Was:" + took + ", expected at least:" + duration, took >= duration);
+    }
 }
