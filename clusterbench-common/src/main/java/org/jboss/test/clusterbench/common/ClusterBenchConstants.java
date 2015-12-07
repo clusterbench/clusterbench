5ee7dfc319a7fc084ba7ce8cf524ded718675bc3
Radoslav Husar <radosoft@gmail.com>
2015-12-07 18:24:12 +0100

    Cleanup constants interface

--- clusterbench-common/src/main/java/org/jboss/test/clusterbench/common/ClusterBenchConstants.java
+++ clusterbench-common/src/main/java/org/jboss/test/clusterbench/common/ClusterBenchConstants.java
@@ -18,6 +18,7 @@ package org.jboss.test.clusterbench.common;
 
 public interface ClusterBenchConstants {
 
-    public static final String READONLY = "readonly";
-    public static final String INVALIDATE = "invalidate";
+    String READONLY = "readonly";
+
+    String INVALIDATE = "invalidate";
 }
