71ca87ecf0e8540f368e87b1d880633fa91595f2
Radoslav Husar <radosoft@gmail.com>
2022-10-03 22:02:16 +0200

    Change support policy to supported version per branch; drop versions supported in branch 3.x from main; add support matrix to README.md.

--- clusterbench-common/src/main/java/org/jboss/test/clusterbench/common/debug/AbstractCommonDebugServlet.java
+++ clusterbench-common/src/main/java/org/jboss/test/clusterbench/common/debug/AbstractCommonDebugServlet.java
@@ -23,11 +23,11 @@ import java.util.Enumeration;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 import org.jboss.test.clusterbench.common.ClusterBenchConstants;
 import org.jboss.test.clusterbench.common.SerialBean;
--- clusterbench-common/src/main/java/org/jboss/test/clusterbench/common/session/CommonHttpSessionServlet.java
+++ clusterbench-common/src/main/java/org/jboss/test/clusterbench/common/session/CommonHttpSessionServlet.java
@@ -19,11 +19,11 @@ package org.jboss.test.clusterbench.common.session;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 import org.jboss.test.clusterbench.common.ClusterBenchConstants;
 import org.jboss.test.clusterbench.common.SerialBean;
